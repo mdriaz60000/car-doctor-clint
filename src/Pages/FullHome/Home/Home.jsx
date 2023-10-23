967c50c17562b80e40a34e4ff13ebd92cafcd04e
mdriaz60000 <mohammadriaz60000@gmail.com>
2023-10-23 07:45:21 +0600

    added banner

--- src/Pages/FullHome/Home/Home.jsx
+++ src/Pages/FullHome/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from "../Banner/Banner";
 
 
 
@@ -5,6 +6,7 @@ const Home = () => {
     return (
         <div>
        
+       <Banner></Banner>
            <h3>home is home</h3> 
       
         </div>
